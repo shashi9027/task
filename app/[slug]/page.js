aa1b9cbba9d83267232bb9a443f6397e6104f934
unknown <shashi902794@gmail.com>
2024-03-19 22:33:42 +0530

    timeline-map ui

---
+++ app/[slug]/page.js
@@ -0,0 +1,5 @@
+export default function Research({params}){
+    return(
+        <div className="text-center m-5 text-red-600">This Page is  for {params?.slug} Content </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import ReactFlow, {
   addEdge,
   ConnectionLineType,
@@ -10,7 +10,8 @@ import ReactFlow, {
 import dagre from 'dagre';
 
 import { initialNodes, initialEdges } from '../utils/nodes-edges.js';
-import { Tooltip, Button } from '@mui/material';
+import { Tooltip, Button, Typography, Popover } from '@mui/material';
+import { useRouter } from 'next/navigation';
 
 import 'reactflow/dist/style.css';
 
@@ -58,18 +59,8 @@ const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
 const LayoutFlow = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
-
   const [open, setOpen] = React.useState(false);
-
-  const handleTooltipClose = () => {
-    setOpen(false);
-  };
-
-  const handleTooltipOpen = () => {
-    setOpen(true);
-  };
-
-
+  const [openId, setOpenId] = useState()
   const onConnect = useCallback(
     (params) =>
       setEdges((eds) =>
@@ -77,23 +68,37 @@ const LayoutFlow = () => {
       ),
     []
   );
-  const onLayout = useCallback(
-    (direction) => {
-      const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
-        nodes,
-        edges,
-        direction
-      );
+  // const onLayout = useCallback(
+  //   (direction) => {
+  //     const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+  //       nodes,
+  //       edges,
+  //       direction
+  //     );
 
-      setNodes([...layoutedNodes]);
-      setEdges([...layoutedEdges]);
-    },
-    [nodes, edges]
-  );
+  //     setNodes([...layoutedNodes]);
+  //     setEdges([...layoutedEdges]);
+  //   },
+  //   [nodes, edges]
+  // );
 
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event, node) => {
+    setAnchorEl(event.currentTarget);
+    setOpenId(node.id)
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const openPopover = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+  const router = useRouter()
   return (
     <div style={{ height: "100vh" }}>
-      
+     
         <ReactFlow
           nodes={nodes}
           edges={edges}
@@ -101,9 +106,9 @@ const LayoutFlow = () => {
           onEdgesChange={onEdgesChange}
           onConnect={onConnect}
           connectionLineType={ConnectionLineType.SmoothStep}
-          onNodeMouseEnter={(event, node) => console.log(event, node)}
+          onNodeMouseEnter={(event, node) =>   handleClick(event, node)}
+          
           fitView
-
         >
 
           {/* <Panel position="top-right">
@@ -111,6 +116,28 @@ const LayoutFlow = () => {
         <button onClick={() => onLayout('LR')}>horizontal layout</button>
       </Panel> */}
         </ReactFlow>
+        <Popover
+       
+        id={id}
+        open={openPopover}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center"
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center"
+        }}
+      >
+        <Typography  className='p-5' >The content of the {initialNodes?.find((val)=> val?.id == openId)?.data?.label} Popover.
+        <div className='flex justify-center'>
+        <button className='button' onClick={()=> router.push(initialNodes?.find((val)=> val?.id == openId)?.data?.label)}>Show detail</button>
+        </div></Typography>
+       
+      </Popover>
+     
     </div>
   );
 };
